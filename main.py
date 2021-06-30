e358fa7aee0a2c324b8191768a64ca200284f443
Sufyan-007 <66457037+Sufyan-007@users.noreply.github.com>
2021-06-30 18:52:38 +0530

    DL project

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, Request
 from fastapi.responses import FileResponse,HTMLResponse
 import predict
-import cv2
 import numpy as np
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from typing import List
 app = FastAPI()
 
-@app.post('/files', response_class=HTMLResponse)
-async def create_files(request: Request, files: List[bytes] = File(...)):
-    img = np.fromstring(files[0], np.uint8)
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    predict.predict(img)
-    return FileResponse("pp.jpg")
-    
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
-    content = """
+    #displaying webpage to get image input
+    page = """
     <html>
         <body>
-            <h1> JIET Object Classification Model </h1>
+            <h1>CELEBRITY LOOKALIKE DETECTOR</h1>
             <form action="/files/" method="post" enctype="multipart/form-data">
             <input  name="files" type="file" multiple>
             <input type="submit">
@@ -28,4 +19,13 @@ async def main(request: Request):
         </body>
     </html>
     """
-    return content
+    return page
+
+@app.post('/files', response_class=HTMLResponse)
+async def display(request: Request, files: List[bytes] = File(...)):
+    #recivied image
+    img = np.fromstring(files[0], np.uint8)
+    #Getting the prediction
+    predict.predict(img)
+    return FileResponse("predicted.jpg")
+    
--- predict.py
+++ predict.py
@@ -1,15 +1,25 @@
 import keras
 import cv2
 import numpy as np
-
+from list import C
+# list of celebrities name vs index
 model = keras.models.load_model('model1.h5')
 
 def predict(image):
-    
-    img = cv2.resize(image, dsize = (128, 128))
-    #img = image/ 255
-    cv2.imwrite("pp.jpg",img)
-    classes = ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
-    probs = model.predict(img.reshape(1, 128, 128, 3))[0]
-    print("Predicted: "+ str(np.argmax(probs)) + " with "+  str(round(np.max(probs)*100)) + "% accuracy")
+    # processing image
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, dsize = (128, 128))
+    #predicting result, x is the index value of the celeb
+    res=model.predict(image.reshape(1, 128, 128, 3))[0]
+    x=np.argmax(res)
+    #importing result picture
+    p= 'pics\\'
+    p=p+str(x)+'.jpg'
+    img = cv2.imread(p)
+    img = cv2.resize(img, (1000,800))
+    #adding captions and saving picture to display
+    img=cv2.copyMakeBorder(img,100, 0, 0, 0, cv2.BORDER_CONSTANT)
+    cv2.putText(img,"YOUR CELEBRITY LOOKALIKE",(10,50), cv2.FONT_HERSHEY_COMPLEX,2,(0,255,255),2)
+    cv2.putText(img,C[x].upper(),(50,800), cv2.FONT_HERSHEY_COMPLEX,2.75,(255,255,0),2)
+    cv2.imwrite("predicted.jpg",img)
 
